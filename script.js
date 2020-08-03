b283618220180ad4d970c0a997e61baad178e1fb
Shvets Dmytro <shvetsdmytro90@ukr.net>
2020-08-03 15:01:25 +0300

    Fourth

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
+const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
 
-// function test() {
-//     VoiceRSS.speech({
-//         key: '2e42e213322644e0aeff3e6c0a35d2f1',
-//         src: 'Hello, world!',
-//         hl: 'en-us',
-//         v: 'Linda',
-//         r: 0, 
-//         c: 'mp3',
-//         f: '44khz_16bit_stereo',
-//         ssml: false
-//     });
-// };
-
-// test();
+// Play Joke on Click
+button.onclick = function() {
+    getJoke(apiUrl, 'GET', implementationJoke);
+};
 
 // Get joke from Joke API
-const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
 let joke = '';
-let xhttp = new XMLHttpRequest();
-xhttp.open('GET', apiUrl, true);
-xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xhttp.send();
-
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
-        if (data.setup) {
-            joke = `${data.setup} ... ${data.delivery}`;
+function getJoke (url, method, functionName) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', apiUrl, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            data = JSON.parse(this.responseText);
+            functionName(data);
         }
-        else {
-            joke = data.joke;
-        }
-        console.log(joke);
     }
-}
+};
+
+// Parsing and insert Joke
+function implementationJoke(data) {
+    if (data.setup) {
+        joke = `${data.setup} ... ${data.delivery}`;
+    }
+    else {
+        joke = data.joke;
+    }
+    tellJoke(joke);
+};
+
+// Send Joke to VoiceRSS API
+function tellJoke(voicesText) {
+    VoiceRSS.speech({
+        key: '2e42e213322644e0aeff3e6c0a35d2f1',
+        src: voicesText,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+};
