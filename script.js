e850c97c5c289def88a9a80126ec91292a11bf22
Shvets Dmytro <shvetsdmytro90@ukr.net>
2020-08-03 13:51:06 +0300

    Third

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
 
-function test() {
-    VoiceRSS.speech({
-        key: '2e42e213322644e0aeff3e6c0a35d2f1',
-        src: 'Hello, world!',
-        hl: 'en-us',
-        v: 'Linda',
-        r: 0, 
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
-    });
-};
+// function test() {
+//     VoiceRSS.speech({
+//         key: '2e42e213322644e0aeff3e6c0a35d2f1',
+//         src: 'Hello, world!',
+//         hl: 'en-us',
+//         v: 'Linda',
+//         r: 0, 
+//         c: 'mp3',
+//         f: '44khz_16bit_stereo',
+//         ssml: false
+//     });
+// };
 
-test();
+// test();
+
+// Get joke from Joke API
+const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+let joke = '';
+let xhttp = new XMLHttpRequest();
+xhttp.open('GET', apiUrl, true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttp.send();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText);
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        }
+        else {
+            joke = data.joke;
+        }
+        console.log(joke);
+    }
+}
